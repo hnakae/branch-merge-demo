00cf3f2c19caffe6b72ad374dc56973371e6e141
hnakae <nakae27@gmail.com>
2023-08-29 14:54:17 -0700

    box

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import Box from "./components/box";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         <h1>Hello</h1>
+        <Box />
       </div>
     </main>
   );
